d141030aa035d2353c288dbac6cdd10e8bcc254b
Brandon Eley <brandon@brandoneley.com>
2010-12-03 13:29:39 -0500

    Testing

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8 />
+	<title>Hi Brant!</title>
+	
+	<link rel="stylesheet" type="text/css" media="all" href="/css/reset.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="/css/text.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="/css/960.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="/css/master.css" />
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div id="container" class="container_16">
+	<h1>
+		16 Column Grid
+	</h1>
+	<div class="grid_16">
+		<p>
+			Hey Brant
+		</p>
+	</div>
+	<!-- end .grid_16 -->
+</div>
+</body>
+</html>
